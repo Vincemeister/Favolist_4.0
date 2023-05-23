2f0bb8d66448ef55d6b64070c684f46c0eba65b6
Vincent Rehms <v.rehms@gmail.com>
2023-05-23 10:48:31 +0800

    Created the user model and ran the respective migration. Replies are a self-referenced field in comments, without an additional model. Also added some guideline comments on the user card (no effect on code)

---
+++ app/models/comment.rb
@@ -0,0 +1,6 @@
+class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :product
+  has_many :replies, class_name: 'Comment', foreign_key: 'parent_comment_id', dependent: :destroy
+  belongs_to :parent, class_name: 'Comment', optional: true
+end
---
+++ db/migrate/20230523023658_create_comments.rb
@@ -0,0 +1,13 @@
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.text :body
+      t.references :user, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: true
+      t.integer :parent_comment
+
+      t.timestamps
+    end
+  end
+end
+
