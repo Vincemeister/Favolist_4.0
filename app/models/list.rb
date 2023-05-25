91c6645c21a9fe12c6e2b1b9574f43c1a17c7439
Vincent Rehms <v.rehms@gmail.com>
2023-05-25 21:23:30 +0800

    created a content_box for replies, along with a scss for that. Also created a dedicated scss file for forms. Modified the list so I can run list.referrals. Tweaked the comment and replies a little bit. Also tweaked the headers, calling formerly nav_header_list to a generic nav_header, in which different title elements can be plugged in dependign on the page. Definitely not finished for the comment/reply functionality and styling. Still need to make the reply form render properly.

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,6 +3,7 @@ class List < ApplicationRecord
 
   belongs_to :user
   has_many :products, dependent: :destroy
+  has_many :referrals, through: :products
 
   pg_search_scope :search_by_name_and_description_and_product_title_and_user_username,
   against: [:name, :description],
