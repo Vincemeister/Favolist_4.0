cd169bf02f85c3b41bb58652badbc315c7bc8ffa
Vincent Rehms <v.rehms@gmail.com>
2023-06-23 19:10:30 +0800

    MIGRATION: Added privacy column to users

---
+++ db/migrate/20230623110943_add_privacy_to_users.rb
@@ -0,0 +1,5 @@
+class AddPrivacyToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :privacy, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_22_104650) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_23_110943) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -127,6 +127,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_22_104650) do
     t.string "username"
     t.text "bio"
     t.integer "followers_count"
+    t.string "privacy"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
