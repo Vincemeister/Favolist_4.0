894ba280b06e2a4d0452258924d2658b27a7e04f
Vincent Rehms <v.rehms@gmail.com>
2023-07-08 19:04:01 +0800

    dynamic background now almost complete. some small adjustments necessary

--- app/javascript/controllers/slidingimagerows_controller.js
+++ app/javascript/controllers/slidingimagerows_controller.js
@@ -21,10 +21,17 @@ export default class extends Controller {
       const row = this.rowTargets[index]
       if (!row) return;
 
-      const imageElements = images.map(imageUrl => `<img class="uniform" style="width: ${100/images.length}%" src="${imageUrl}" />`).join('')
+      // Get the height of the row
+      const rowHeight = row.offsetHeight;
+
+      // Calculate the width of the images based on the row's aspect ratio
+      const imageWidth = rowHeight;  // Since we want a 1:1 aspect ratio
+
+      const imageElements = images.map(imageUrl => `<img class="uniform" style="width: ${imageWidth}px; height: ${imageWidth}px;" src="${imageUrl}" />`).join('')
       row.innerHTML = `<div class="sliding-row-content">${imageElements + imageElements}</div>`
     })
   }
+
 
 
 }
