ffec1148d40649e55c1f3b01980d195c0ace3f10
Vincent Rehms <v.rehms@gmail.com>
2023-07-28 22:49:13 +0800

    devised the partial seed code to create people and lists from csv file. Also ammended the storage.yml file. For some reason cloudinary had a P in front of where it shouldnt

---
+++ db/data_loader.rb
@@ -0,0 +1,52 @@
+require "csv"
+
+
+
+# people_csv = "./db/data/people.csv"
+# i = 0
+# CSV.foreach(people_csv, headers: :first_row, header_converters: :symbol) do |row|
+#   puts "----------------- ROW #{i} -----------------"
+#   user = User.create!(
+#     username: row[:profile],
+#     bio: row[:bio],
+#     email: "#{row[:profile].gsub(/\s+/, '')}@gmail.com",
+#     password: "password"
+#   )
+#   puts user
+#   i += 1
+#   break if i > 5
+# end
+
+# User.last(5).each do |user|
+#   puts "----------------- USER -----------------"
+#   puts user
+#   puts user.username
+#   puts user.bio
+# end
+
+
+
+list_csv = "./db/data/lists.csv"
+
+i = 0
+CSV.foreach(list_csv, headers: :first_row, header_converters: :symbol) do |row|
+  puts "----------------- ROW #{i} -----------------"
+
+  p row[:profile]
+
+  list = List.new(
+    name: row[:list],
+    description: row[:info],
+  )
+
+  list.user = User.find_by(username: row[:profile])
+  list.save!
+
+  puts list
+  puts list.name
+  puts list.description
+  puts list.user.username
+
+  i += 1
+  break if i > 5
+end
