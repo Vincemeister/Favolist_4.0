339f4f89860d8545a6f2c95bf5afd95712e33508
Vincent Rehms <v.rehms@gmail.com>
2023-07-29 20:03:24 +0800

    improved data_loader, can almost use it compeltely. Product section missing.

--- db/data_loader.rb
+++ db/data_loader.rb
@@ -1,30 +1,39 @@
 require "csv"
 
+People.last(5).each do |person|
+  person.destroy
+  puts "check that people are destroyed"
+  puts People.last
+end
+
+people_csv = "./db/data/people.csv"
+i = 0
+CSV.foreach(people_csv, headers: :first_row, header_converters: :symbol) do |row|
+  puts "----------------- ROW #{i} -----------------"
+  user = User.create!(
+    username: row[:profile],
+    bio: row[:bio],
+    email: "#{row[:profile].gsub(/\s+/, '')}@gmail.com",
+    password: "password"
+  )
+  puts user
+  i += 1
+  break if i > 5
+end
+
+User.last(5).each do |user|
+  puts "----------------- USER -----------------"
+  puts user
+  puts user.username
+  puts user.bio
+end
 
 
-# people_csv = "./db/data/people.csv"
-# i = 0
-# CSV.foreach(people_csv, headers: :first_row, header_converters: :symbol) do |row|
-#   puts "----------------- ROW #{i} -----------------"
-#   user = User.create!(
-#     username: row[:profile],
-#     bio: row[:bio],
-#     email: "#{row[:profile].gsub(/\s+/, '')}@gmail.com",
-#     password: "password"
-#   )
-#   puts user
-#   i += 1
-#   break if i > 5
-# end
-
-# User.last(5).each do |user|
-#   puts "----------------- USER -----------------"
-#   puts user
-#   puts user.username
-#   puts user.bio
-# end
-
-
+List.last(5).each do |list|
+  list.destroy
+  puts "check that lists are destroyed"
+  puts List.last
+end
 
 list_csv = "./db/data/lists.csv"
 
@@ -50,3 +59,5 @@ CSV.foreach(list_csv, headers: :first_row, header_converters: :symbol) do |row|
   i += 1
   break if i > 5
 end
+
+
