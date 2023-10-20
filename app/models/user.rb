0531f14a7e7262970bf587bd81e88e4f784aa3fa
Vincent Rehms <v.rehms@gmail.com>
2023-10-20 12:56:42 +0800

    increased bio length to 400 characters

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :username, presence: true, uniqueness: true
 
-  validates :intro, length: { maximum: 280 }
+  validates :intro, length: { maximum: 400 }
   validates :bio, length: { maximum: 5000 }
 
 
